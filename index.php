ca693b380b4a509d0fe266009a749dfc305c226a
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-30 14:56:49 +0530

    ADDED index.php FILE

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alpha Notes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="" method="post">
+        <div class="">
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" placeholder="e.g. My Favourite Comedy Movies">
+        </div>
+        <div class="">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" placeholder="1. Phir Hera Pheri
+2. Houseful 3 
+3. Its Entertainment"></textarea>
+        </div>
+        <button type="submit">Add Note</button>
+    </form>
+</body>
+
+</html>
